5aa7d8179a24bdaf5359d937a5685553ed219558
Ivan Gochev <redial@abv.bg>
2020-03-30 11:27:29 +0300

    Added functions to Player. Added tests

--- classes/player.py
+++ classes/player.py
@@ -1,7 +1,6 @@
-from utils.mixins import Jsonable
-
-import itertools
+from mixins import Jsonable
 from CardsBelote import Card, Deck
+import itertools
 
 
 class Player(Jsonable):
@@ -45,61 +44,71 @@ class Player(Jsonable):
     def add_round_report(self):
         self.announcements.extend(self.round_report)
 
-    def check_for_announcements(self):
-        annsCarre = []
-        annsConsec  = []
-        annsBelote = []
+    def check_for_announcements(self,gametype):
+        anns = {}
+
+        #find Belote 
+        def find_belote_anns(gametype):
+            if gametype == "All trumps":
+                for a in self.cards:
+                    if a.value == 'Queen':
+                        b = Card(a.suit, 'King')
+                        if b in self.cards:
+                            anns['belote'] = [a,b]
+            elif gametype in ['Hearts', 'Diamonds', 'Clubs', 'Spades']:
+                for a in self.cards:
+                    if a == Card(gametype, 'Queen'):
+                        b = Card(a.suit, 'King')
+                        if b in self.cards:
+                            anns['belote'] = [a,b]
 
         #find Carre
-        #group same value
-        x = [list(j) for i, j in itertools.groupby(self.cards)]
-        for i in x:
-            if len(i) == 4:
-                if i[0].get_key() in (4,6,7,9):
-                    annsCarre.append("carre of {}'s = 100 points".format(i[0].value))
-                elif i[0].get_key() == 3:
-                    annsCarre.append("carre of {}'s = 150 points".format(i[0].value))
-                elif i[0].get_key() == 5:
-                    annsCarre.append("carre of {}'s = 200 points".format(i[0].value))
-
-        #Groups cards into consecutive values
-        def groupSequence(lst):
-            res = [[lst[0]]]
-            for i in range(1, len(lst)):
-                #get_key used to compare the coresponding value in valuesDict
-                if (lst[i-1]).get_key()+1 == (lst[i]).get_key():
-                    res[-1].append(lst[i])
-                else:
-                    res.append([lst[i]])
-            return res
-
-        groups = groupSequence(self.cards)
+        def find_carre_anns(gametype):
+            if gametype != 'No trumps':
+                #group same value
+                x = [list(j) for i, j in itertools.groupby(self.cards)]
+                for i in x:
+                    if len(i) == 4:
+                        if i[0].get_key() in (4,6,7,9):
+                            anns["carre of {}'s = 100 points".format(i[0].value)] = i
+                        elif i[0].get_key() == 3:
+                            anns["carre of {}'s = 150 points".format(i[0].value)] = i
+                        elif i[0].get_key() == 5:
+                            anns["carre of {}'s = 200 points".format(i[0].value)] = i
+        
         #finds Tierce, Quarte or Quinte
-        groups = groupSequence(self.cards)
-        for grp in groups:
-            if len(grp) == 3:
-                annsConsec.append('tierce')
-            elif len(grp) == 4:
-                annsConsec.append('quarte')
-            elif len(grp) >= 5:
-                annsConsec.append('quinte')
+        def find_consecutive_anns(gametype):
+            if gametype != 'No trumps':
+                #Groups cards into consecutive values
+                def groupSequence(lst):
+                    res = [[lst[0]]]
+                    for i in range(1, len(lst)):
+                        #get_key used to compare the coresponding value in valuesDict
+                        if (lst[i-1]).get_key()+1 == (lst[i]).get_key():
+                            res[-1].append(lst[i])
+                        else:
+                            res.append([lst[i]])
+                    return res
 
-        #find Belote
-        for a in self.cards:
-            if a.value == 'Queen':
-                if Card(a.suit, 'King') in self.cards:
-                    annsBelote.append('belote')
+                groups = groupSequence(self.cards)
+                for grp in groups:
+                    if len(grp) == 3:
+                        anns['tierce'] = grp
+                    elif len(grp) == 4:
+                        anns['quarte'] = grp
+                    elif len(grp) >= 5:
+                        anns['quinte'] = grp
 
+        find_carre_anns(gametype)
+        find_consecutive_anns(gametype)
+        find_belote_anns(gametype)
+        self.round_report  = anns
 
-
-        self.announcements.append(annsCarre)
-        self.announcements.append(annsConsec)
-        self.announcements.append(annsBelote)
 
 
 
 def main():
     pass
-
+    
 if __name__ == "__main__":
     main()
